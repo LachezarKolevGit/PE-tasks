21723d347767e77b71b26cd7ab3ed3118a53a917
Lachezar Kolev <76496265+LachezarKolevGit@users.noreply.github.com>
2022-11-23 18:01:27 +0200

    Lab 1, task 3
    Създайте клас Line, който чертае линия на екрана, чрез изобразяване на линията с 
    помощта на *. Съхранете дължината на линията в private променлива Len от тип 
    Integer. Нека конструкторът на Line приема един параметър: дължината на линията. 
    Нека конструкторът да изчертава линията и да съхранява нейната дължина. Създайте 
    деструктор, който да изтрива линията.

---
+++ lineLab1/Line.cpp
@@ -0,0 +1,20 @@
+#include "Line.h"
+
+Line::Line(int lineLenght) {
+	std::cout << "Enter length of line"<<std::endl;
+	std::cin >> lineLenght;
+	len=lineLenght;
+	for (int i = 0; i < lineLenght; i++)
+	{
+		std::cout <<"*" ;
+	}
+
+
+
+}
+Line::~Line() {
+	system("CLS");
+	std::cout << "destructor madafaka";
+
+
+}
---
+++ lineLab1/Line.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+class Line {
+public:
+	Line(int);
+	~Line();
+
+private:
+	int len;
+
+};
