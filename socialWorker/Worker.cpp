67c9dcaa06972bbbab5f4a9b56bf74f166bcb3e4
Lachezar Kolev <76496265+LachezarKolevGit@users.noreply.github.com>
2022-11-23 18:08:18 +0200

    Lab 1, task 2
    Създайте клас, който описва обекта работник със следните характеристики: социален 
    номер, име, години трудов стаж, текуща длъжност и списък от взимани заплати (тип 
    данни - масив). 
    Забележка: Нивото на достъп до член - променливите да е private.
    Класът има два конструктора:
    Инициализира социалният номер и годините трудов стаж с нулева стойност.
    Инициализиране на текуща длъжност да се въведе от клавиатурата.
    Класът съдържа следните член - функции:
    Задаване и получаване на стойности за член-променливите.
    За пресмятане на средно аритметичната стойност от заплатите.
    За намиране на минимална заплата.
    Демонстрирайте функционалността на класа чрез създаване на обект в main().

---
+++ socialWorker/Worker.cpp
@@ -0,0 +1,112 @@
+#include "Worker.h"
+
+
+Worker::Worker() {
+	std::cout << "Initializing id and work expirience" << "\n";
+	workingExpirience = 0;
+	id ="" ; 
+	std::cout << "Enter your age:" << std::endl;
+	
+	std::cin >> age;
+	std::cout << "Enter you name:" << std::endl;
+	std::cin >> name;
+	std::cout << "Enter list of salaries" << std::endl;
+	std::cout << "If you are done with your list of salaries enter: -1" << std::endl;
+	int i = 0;
+	int salary;
+	salaryList.reserve(50);
+	while (i<50)
+	{
+		
+		std::cin >> salary;
+		if (salary == -1) {
+			break;
+		}
+		salaryList.push_back(salary);
+		
+		i++;
+	}
+
+		
+}
+
+Worker::Worker(String jobInput):Worker() {
+	std::cout << "Enter your current job" << std::endl;
+	std::cin >> jobInput;
+	currentJob = jobInput;
+	std::cout <<"Job saved" << "\n";
+	
+
+
+}
+
+
+
+String Worker:: getId() {
+	return id;
+}
+
+String Worker::getName() {
+	return name;
+}
+
+int Worker::getAge() {
+	return age;
+}
+
+int Worker::getWorkingExpirience() {
+	return workingExpirience;
+}
+String Worker::getCurrentJob() {
+	return currentJob;
+}
+void Worker::setId(String inputId) {
+	id=inputId;
+}
+void Worker::setName(String inputName) {
+	name = inputName;
+}
+void Worker::setAge(int inputAge) {
+	age = age;
+}
+void Worker::setWorkingExpirience(int expirience) {
+	workingExpirience = expirience;
+}
+void Worker::setCurrentJob(String job) {
+	currentJob = job;
+}
+int Worker::calcAverageSalary() {
+	int averageSalary = 0;
+	int i = 0;
+	int sum=0;
+	while (i<salaryList.size())
+	{
+		sum += salaryList[i];
+
+		i++;
+	}
+	return sum / salaryList.size();
+}
+int Worker::calcMinSalary() {
+	int min = salaryList[0];
+	int i= 0;
+	while (i<salaryList.size())
+	{
+		if (min > salaryList[i])
+			min = salaryList[i];
+			
+	
+		i++;
+	}
+
+	return min;
+
+}
+std::vector<int> Worker::getListOfSalaries() {
+	return salaryList;  
+
+}
+void Worker::setListOfSalaries(std::vector<int> list) {
+	salaryList = list;
+
+}
